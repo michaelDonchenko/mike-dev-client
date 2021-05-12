42f6f5996b6618930caeb32b5fb77da361f16dec
michaelDonchenko <michael1994d@gmail.com>
2021-05-12 10:52:28 +0300

    added new project

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,7 +1,16 @@
 import olgaJewelry from '../images/olgaJewelry.PNG'
 import mikesBlog from '../images/mikesBlog.PNG'
+import travelApp from '../images/travelApp.PNG'
 
 const projects = [
+  {
+    projectTitle: 'Travel app',
+    projectText:
+      'Website created with MERN stack and redux toolkit for state management. Full auth system with jwt and cookies, register login and forgot password with email reset link, non registered user can view all markers and places on the map. Logged in users are able to add thier own markers on the map and add photos/rating/comments to other people markers. Only admin user can delete markers witch will delete all related info to the marker.',
+    img: travelApp,
+    demoLink: 'https://mike-travel.netlify.app',
+    githubLink: 'https://github.com/michaelDonchenko/travel-app-client',
+  },
   {
     projectTitle: 'Social media blog',
     projectText:
